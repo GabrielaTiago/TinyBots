a62e03e8478a73b8012469d6bae98a82bd5c2f40
Gabriela Tiago de Araújo <gabrielatiagodearaujo@outlook.com>
2024-05-22 21:53:00 -0300

    ✨ feat: Implementa bot para tirar prints

---
+++ pyautogui_bots/tirar_prints/app.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+# Tirar prints da tela
+
+# Tela inteira
+# pyautogui.screenshot('tela_inteira.png')
+
+# Tirar print de uma janela específica
+pyautogui.screenshot('calculadora.png', region=(274,47, 320, 630))
