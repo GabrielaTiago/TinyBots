be73a630453f52d8cf32c5049e455c97839c1fce
Gabriela Tiago de Araújo <gabrielatiagodearaujo@outlook.com>
2024-06-11 12:21:57 -0300

    ✨ feat: Adiciona pipeline para armazenar citações em um banco de dados SQLite

--- scrapy_bots/coleta_citacoes/coleta_citacoes/pipelines.py
+++ scrapy_bots/coleta_citacoes/coleta_citacoes/pipelines.py
@@ -4,10 +4,35 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import sqlite3
+
+from coleta_citacoes.items import QuoteItem
+from scrapy.exceptions import DropItem
 
 
-class ColetaCitacoesPipeline:
+class SQLitePipeline:
+    def open_spider(self, spider):
+        self.connection = sqlite3.connect('quotes.db')
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS quotes(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                quote TEXT,
+                author TEXT,
+                tags TEXT
+            )
+        ''')
+        self.connection.commit()
+
+    def close_spider(self, spider):
+        self.connection.close()
+
     def process_item(self, item, spider):
-        return item
+        if isinstance(item, QuoteItem):
+            self.cursor.execute('''
+                INSERT INTO quotes(quote, author, tags) VALUES(?, ?, ?)
+            ''', (item['quote'], item['author'], item['tags']))
+            self.connection.commit()
+            return item
+        else:
+            raise DropItem('Item not recognized', item)
--- scrapy_bots/coleta_citacoes/coleta_citacoes/settings.py
+++ scrapy_bots/coleta_citacoes/coleta_citacoes/settings.py
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "coleta_citacoes.pipelines.ColetaCitacoesPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "coleta_citacoes.pipelines.SQLitePipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
